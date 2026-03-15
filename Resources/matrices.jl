a13eccfabfebfea3a7607caa3a25bbab9a4b969f
Rubelito Abella <bbadoodles@gmail.com>
2026-03-16 07:00:07 +0800

    update matrix solutions

--- Resources/matrices.jl
+++ Resources/matrices.jl
@@ -8,42 +8,51 @@ using InteractiveUtils
 using LinearAlgebra
 
 # ╔═╡ ef5fac36-ce31-4a2a-bd9c-a667ad812a6e
-# ╠═╡ disabled = true
-#=╠═╡
-A = [[1 -1 -2]
+matA = [[1 -1 -2]
 	[2 3 -1]
 	[-2 1 2]];
-  ╠═╡ =#
 
 # ╔═╡ 374aa4eb-46c1-4b14-a4d0-ac4d01ff33fc
-# ╠═╡ disabled = true
-#=╠═╡
-B = [[2 0]
+matB = [[2 0]
 	[-1 0]
 	[1 1]];
-  ╠═╡ =#
 
 # ╔═╡ b9134d99-add8-42f9-a22b-9466a879fdcb
-# ╠═╡ disabled = true
-#=╠═╡
-C = [[3 2]
+matC = [[3 2]
 	[-1 2]];
-  ╠═╡ =#
 
 # ╔═╡ cf3008eb-de7b-4d57-b0d1-c0ce14ba5139
-# ╠═╡ disabled = true
-#=╠═╡
-D = [[4  0.5]
+matD = [[4  0.5]
 	 [-2 -1]];
-  ╠═╡ =#
 
 # ╔═╡ cd404059-7975-4ee8-b15b-d15cbf3988ff
-# ╠═╡ disabled = true
-#=╠═╡
-E = [[-1.5 1 -0.5]
+matE = [[-1.5 1 -0.5]
 	[4 -2 1]
 	[-0.5 0 -0.5]];
-  ╠═╡ =#
+
+# ╔═╡ 8e90e10e-06b3-4bbd-b97c-96769b4c70b3
+#1
+matA + matB
+
+# ╔═╡ f36f03c1-805f-41c2-91ea-05952aeabd2a
+#2
+matA + 2matE
+
+# ╔═╡ fa92f42e-4bd0-4be3-b700-18442901e58b
+#3
+matB * matC
+
+# ╔═╡ 9744fc45-7ff9-49f1-8e83-4e8354f271a1
+#4
+matC * matB
+
+# ╔═╡ fe0270b9-bfa0-4305-804a-a324823af999
+#5
+(matB * matC) + matB
+
+# ╔═╡ a3658e1d-152a-4ec4-bf15-f3454c55e1a1
+#6
+matD * matB'
 
 # ╔═╡ 2f342dcf-ddb4-44ed-b27c-24c7e7cbffb1
 begin
@@ -92,30 +101,6 @@ begin
 	[4 -2 1]
 	[-0.5 0 -0.5]];
 end
-
-# ╔═╡ 8e90e10e-06b3-4bbd-b97c-96769b4c70b3
-#1
-A + B
-
-# ╔═╡ f36f03c1-805f-41c2-91ea-05952aeabd2a
-#2
-A + 2E
-
-# ╔═╡ fa92f42e-4bd0-4be3-b700-18442901e58b
-#3
-B * C
-
-# ╔═╡ 9744fc45-7ff9-49f1-8e83-4e8354f271a1
-#4
-C * B
-
-# ╔═╡ fe0270b9-bfa0-4305-804a-a324823af999
-#4
-(B * C) + B
-
-# ╔═╡ a3658e1d-152a-4ec4-bf15-f3454c55e1a1
-#6
-D * B'
 
 # ╔═╡ 34c080c2-a349-4803-a84f-527931572a89
 det(A)
